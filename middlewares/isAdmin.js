bf0bfe29a74ea5fc8841bb8fb440f0a4890213b2
zephrod <valentin.le_fessant@outlouk.fr>
2024-12-22 16:45:28 +0100

    export ROLES pour eviter admin en claire

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,6 +1,7 @@
+const ROLES = require("../models/roles");
 module.exports = () => {
   return (req, res, next) => {
-    if (req.user.role !== 'admin') {
+    if (req.user.role !== ROLES[1]) {
       return res.status(403);
     }
     next();
